7a638f7bd99131599f02bb3f6acf96a860cbf204
suvarnamerp-cyber <suvarnamerp@gmail.com>
2026-02-18 14:42:03 +0530

    Create CorsConfig.java

---
+++ src/main/java/com/example/ankush/config/CorsConfig.java
@@ -0,0 +1,25 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+ 
+@Configuration
+public class CorsConfig {
+ 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:3001",
+                                "http://192.168.1.32:3001"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
